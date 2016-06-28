4bb42c295678519168ea2071a1526a4f3c0bdd91
Peter Cutler <peter@crowdskout.com>
2016-06-28 13:59:33 -0400

    updated with angular datetime insertion

--- jquery.datetimepicker.js
+++ jquery.datetimepicker.js
@@ -915,7 +915,8 @@
 					'<div class="xdsoft_label xdsoft_year"><span></span><i></i></div>' +
 					'<button type="button" class="xdsoft_next"></button></div>'),
 				calendar = $('<div class="xdsoft_calendar"></div>'),
-				timepicker = $('<div class="xdsoft_timepicker active"><button type="button" class="xdsoft_prev"></button><div class="xdsoft_time_box"></div><button type="button" class="xdsoft_next"></button></div>'),
+				// timepicker = $('<div class="xdsoft_timepicker active"><button type="button" class="xdsoft_prev"></button><div class="xdsoft_time_box"></div><button type="button" class="xdsoft_next"></button></div>'),
+        timepicker = $('<div class="xdsoft_timepicker active"><input type="text" datetime="yyyy-MM-dd" ng-model="myDate"></div>'),
 				timeboxparent = timepicker.find('.xdsoft_time_box').eq(0),
 				timebox = $('<div class="xdsoft_time_variant"></div>'),
 				applyButton = $('<button type="button" class="xdsoft_save_selected blue-gradient-button">Save Selected</button>'),
